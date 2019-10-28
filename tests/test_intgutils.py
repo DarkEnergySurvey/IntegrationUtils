1a3f8e228b6a82a486791017a1e472803b7f12c8
Doug Friedel <friedel@illinois.edu>
2019-10-28 11:13:13 -0500

    full wcl test

--- tests/test_intgutils.py
+++ tests/test_intgutils.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python2
 
 import unittest
+import filecmp
+import os
 
 import intgutils.intgmisc as igm
+import intgutils.wcl as wcl
 
 ROOT = '/var/lib/jenkins/test_data/'
 
@@ -15,6 +18,19 @@ class TestIntgmisc(unittest.TestCase):
         self.assertTrue('test_raw.fits' in exist[0])
         self.assertTrue('notthere' in missing[0])
 
+class TestWCL(unittest.TestCase):
+    def test_init(self):
+        try:
+            os.unlink('out.wcl')
+        except:
+            pass
+        fl = ROOT + 'wcl/TEST_DATA_r15p03_full_config.des'
+        w = wcl.WCL()
+        with open(fl, 'r') as infh:
+            w.read(infh, fl)
+        self.assertEqual(w['attnum'], str(3))
+        w.write(open('out.wcl', 'w'))
+        self.assertTrue(filecmp.cmp(fl, 'out.wcl'))
 
 if __name__ == '__main__':
     unittest.main()
